a2a6d8e62e2e3c51ab590ec2cc7e56b8101d32b4
Luke <imlukedewitt@gmail.com>
2022-08-26 16:32:52 -0500

    Initial commit

---
+++ psp-theme-generator/pspgen2.js
@@ -0,0 +1,24 @@
+let config = {
+    darkmode: true,
+    layout: "default",
+    googleFont: "default",
+    colors: {
+        accent: "#ff3996",
+        hover: "",
+        background: "default"
+    },
+    requireOrg: false,
+    requirePhone: false,
+    requireCustomFields: [],
+    hideCustomFields: [],
+    hideRecurringFee: false,
+    hideComponentFee: false,
+    hidePhone: false,
+    hideOrg: false,
+    hideHeader: false,
+    hideNoneOption: false,
+    replaceText: [],
+    debugMode: false,
+    livePreview: false
+};
+
