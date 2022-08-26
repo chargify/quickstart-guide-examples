f3c6be698334fd7b076bc6b5eb72260fa65b0de3
Luke <imlukedewitt@gmail.com>
2022-08-26 16:33:34 -0500

    add accent/hover/background color CSS

--- psp-theme-generator/pspgen2.js
+++ psp-theme-generator/pspgen2.js
@@ -22,3 +22,27 @@ let config = {
     livePreview: false
 };
 
+function setAccentColors(accent, hover = accent) {
+    let css = `
+    #subscription_submit, #form__section-apply-components, .form__button--primary, .btn-success {
+        border: solid 2px ${accent};
+        background-color: ${accent};
+        color: '#FFF';
+    }
+    #form__section-apply-components:hover, #subscription_submit:hover, .form__button--is-submitting,
+    .form__button--primary:hover, .form__button--primary:active, .form__button--primary:focus,
+    .form__button--primary:disabled, .btn-success:hover {
+        background-color: ${hover};
+        border: solid 2px ${hover};
+        color: '#FFF';
+        background-image: none;
+    }
+    .plan__summary-total { color: ${accent}; }
+    .content__heading--mobile .content__heading-section--total {
+        color: #ffffff;
+    }
+    `.replace(/^ +/gm, '');
+    $("<style>").prop("type", "text/css").html(css).appendTo("head");
+}
+
+setAccentColors(config.colors.accent);
