74694bd74d4b327dee22f1084df0583d61e5514e
Luke <imlukedewitt@gmail.com>
2022-08-29 13:40:51 -0500

    condense accentColor css

--- psp-theme-generator/pspgen2.js
+++ psp-theme-generator/pspgen2.js
@@ -20,26 +20,23 @@ let config = {
     livePreview: false
 };
 
-function setAccentColors(accent, hover = accent) {
+function applyAccentColors(accent, hover = accent) {
     let css = `
     #subscription_submit, #form__section-apply-components, .form__button--primary, .btn-success {
         border: solid 2px ${accent};
         background-color: ${accent};
-        color: '#FFF';
-    }
+        color: '#FFF'; }
     #form__section-apply-components:hover, #subscription_submit:hover, .form__button--is-submitting,
     .form__button--primary:hover, .form__button--primary:active, .form__button--primary:focus,
     .form__button--primary:disabled, .btn-success:hover {
         background-color: ${hover};
         border: solid 2px ${hover};
         color: '#FFF';
-        background-image: none;
-    }
+        background-image: none; }
     .plan__summary-total { color: ${accent}; }
     .content__heading--mobile .content__heading-section--total {
-        color: #ffffff;
-    }
-    `.replace(/^ +/gm, '');
+        color: #ffffff; }
+    `;
     $("<style>").prop("type", "text/css").html(css).appendTo("head");
 }
 
