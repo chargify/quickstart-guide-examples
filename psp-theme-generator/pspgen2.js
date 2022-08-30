a3ea40dd8eafc6936823334a455974938f829326
Luke <imlukedewitt@gmail.com>
2022-08-30 08:16:38 -0500

    move default layout to separate function

--- psp-theme-generator/pspgen2.js
+++ psp-theme-generator/pspgen2.js
@@ -1,6 +1,6 @@
 let config = {
-    darkMode: false,
-    layout: "minimal",
+    darkMode: true,
+    layout: "default",
     googleFont: "",
     accentColor: "#ff3657",
     hoverColor: "#df304c",
@@ -99,6 +99,35 @@ function minimalLayout(textColor, backgroundColor, borderColor) {
     `
 }
 
+function defaultLayout(textColor, backgroundColor, borderColor) {
+    return `
+    body {background-color: ${backgroundColor};}
+    .header, .header h1, .footer__operated-by-link, .footer__privacy-policy-link, h2, h3, h4 {
+        color: ${textColor} }
+    .content, .content__main, .form__field-radio-group {
+        background-color: #232a31;
+        color: ${textColor};
+        border: 0px solid ${borderColor}; }
+    .content__headings, .content__heading--mobile, .form__section--credit-card header, .form__section--configure-plan .form__field--boxed, .form__section--billing-address .form__field-radio-group .radio header {
+        background: ${backgroundColor};
+        border: 1px solid ${borderColor} }
+    .form__section--boxed header {
+        border: 0px;
+        border-bottom: 1px solid ${borderColor};
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px; }
+    .content__heading {
+        background: ${backgroundColor};
+        border-bottom: 0px solid ${borderColor}; }
+    .form__field-radio-group .radio:not(:last-child) header {
+        border-bottom: 1px solid ${borderColor} }
+    .form__section--credit-card {
+        border: 1px solid ${borderColor} }
+    .content__main {
+        border-right: 1px solid ${borderColor} }
+`
+}
+
 function applyLayout(layout, darkMode, backgroundColor) {
     let textColor = darkMode ? '#f9f9f9' : '#000000';
     let borderColor = darkMode ? '#ffffff30' : '#00000030';
@@ -112,38 +141,10 @@ function applyLayout(layout, darkMode, backgroundColor) {
         default:
             defaultBackgroundColor = darkMode ? "#181e23" : "#f9f9f9";
             if (backgroundColor.length == 0 ) backgroundColor = defaultBackgroundColor;
-            css = `body {background-color: ${backgroundColor};}`;
-            console.log(css)
-            if (!darkMode) break;
-
-            css += `
-                .header, .header h1, .footer__operated-by-link, .footer__privacy-policy-link, h2, h3, h4 {
-                    color: #f9f9f9 }
-                .content, .content__main, .form__field-radio-group {
-                    background-color: #232a31;
-                    color: #f9f9f9;
-                    border: 0px solid ${borderColor}; }
-                .content__headings, .content__heading--mobile, .form__section--credit-card header, .form__section--configure-plan .form__field--boxed, .form__section--billing-address .form__field-radio-group .radio header {
-                    background: #38434f;
-                    border: 1px solid ${borderColor} }
-                .form__section--boxed header {
-                    border: 0px;
-                    border-bottom: 1px solid ${borderColor};
-                    border-top-left-radius: 4px;
-                    border-top-right-radius: 4px; }
-                .content__heading {
-                    background: #38434f;
-                    border-bottom: 0px solid ${borderColor}; }
-                .form__field-radio-group .radio:not(:last-child) header {
-                    border-bottom: 1px solid ${borderColor} }
-                .form__section--credit-card {
-                    border: 1px solid ${borderColor} }
-                .content__main {
-                    border-right: 1px solid ${borderColor} }
-            `;
+            css = defaultLayout(textColor, backgroundColor, borderColor);
             break;
     };
-    css = css.replace(/^ +/gm, '');
+    css = darkMode ? css.replace(/^ +/gm, '') : `body {background-color: ${backgroundColor};}`;
     $("<style>").prop("type", "text/css").html(css).appendTo("head");
 }
 
